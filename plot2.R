4a6d38a5d4ab8be39b59205e42b000388bfd5ee3
casassus <casassus@hotmail.com>
2014-05-11 20:06:00 -0400

    commit2
    PNG and R script

--- plot2.R
+++ plot2.R
@@ -3,20 +3,18 @@ datos<-read.csv("household_power_consumption.txt",sep=";", dec = ".", stringsAsF
 datos<-datos[datos$Date=="1/2/2007" | datos$Date=="2/2/2007" ,]
 
 ## subsetting
-data1<-datos["Date"]
-data3<-datos["Time"]
 
-data1<- paste(data1[1],data3[1],  sep = " ")
+data<-data.frame(paste(datos[,"Date"],datos[,"Time"]),stringsAsFactors=FALSE)
+
 data2<-datos["Global_active_power"]
 
 
-data1[,1]<-as.Date(data1[,1], format="%d/%m/%Y %H:%M:%S")
+data1<-strptime(data[,1], format="%d/%m/%Y %H:%M:%S")
 data2[,1]<-as.numeric(data2[,1])
 
-x<-data1[,1]
+
 y<-data2[,1]
 
-data<-cbind(data1,data2)
 
 ## plot 
-plot(x,y, ylab="Global Active Power (kilowatts)", xlab="",main="" )
+plot(data1,y, type="l", ylab="Global Active Power (kilowatts)", xlab="",main="" )
